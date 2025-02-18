8c11c886ceaef895978ac2cb022a619e4ff0a110
temirhanb <temirhan023@gmail.com>
2025-02-18 22:06:15 +0500

    - prod port

--- src/pages/chat/hooks/sendMessage/index.ts
+++ src/pages/chat/hooks/sendMessage/index.ts
@@ -9,7 +9,7 @@ let socket;
 if (process.env.NODE_ENV === "development") {
   socket = io("localhost:3000");
 } else {
-  socket = io("https://mustangmidnight-jsy4rm--73342.stormkit.dev/");
+  socket = io("https://whats-app-bot-fc9s.onrender.com:10000");
 }
 
 interface IFormInput {
