0ef071d2cdb7ec099a0627c212c3140b508ea8a5
temirhanb <temirhan023@gmail.com>
2025-02-18 22:16:08 +0500

    -new path

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ const io = new Server(server, {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "../dist")));
+  app.use(express.static(path.join(__dirname1, "dist/")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
+    res.sendFile(path.resolve(process.cwd(), "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
